13f1a8b349a386194260206ea3dd2056e57499ac
matt <matt@ea41ed52-d2ee-0310-a9c1-e6b18d33e121>
2006-08-13 17:13:50 +0000

    Fixed access to null pointer on Cmd-Q.
    git-svn-id: http://seriss.com/public/fltk/fltk/branches/branch-D-1.1@5299 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- D/fl/mac.d
+++ D/fl/mac.d
@@ -1292,7 +1292,7 @@ static OSErr QuitAppleEventHandler(AppleEventRef appleEvt, AppleEventRef reply, 
   while ( Fl_X.first ) {
     Fl_X x = Fl_X.first;
     Fl.handle( FL_CLOSE, x.w );
-    if ( Fl_X.first == x ) {
+    if ( Fl_X.first && Fl_X.first == x ) {
       fl_unlock_function();
       return noErr; // FLTK has not close all windows, so we return to the main program now
     }
