528499f671c2dd7c273e2842706cd4e627d2cec1
fabien <fabien@ea41ed52-d2ee-0310-a9c1-e6b18d33e121>
2008-09-23 22:43:49 +0000

    more fixes and cleanup on quartz.
    git-svn-id: http://seriss.com/public/fltk/fltk/branches/branch-1.3-cairo@6343 ea41ed52-d2ee-0310-a9c1-e6b18d33e121

--- src/Fl_Double_Window.cxx
+++ src/Fl_Double_Window.cxx
@@ -301,9 +301,6 @@ void fl_begin_offscreen(Fl_Offscreen ctx) {
   //fl_push_no_clip();
   CGContextSaveGState(fl_gc);
   Fl_X::q_fill_context();
-#if defined(HAVE_CAIRO) && defined (__APPLE_QUARTZ__)
-  Fl::cairo_make_current(fl_gc); // capture gc changes automatically to update the cairo context adequately
-#endif
 }
 
 void fl_end_offscreen() {
@@ -316,10 +313,6 @@ void fl_end_offscreen() {
   if (stack_ix<stack_max) {
     fl_gc = stack_gc[stack_ix];
     fl_window = stack_window[stack_ix];
-
-#if defined(HAVE_CAIRO) && defined (__APPLE_QUARTZ__)
-    Fl::cairo_make_current(fl_gc); // capture gc changes automatically to update the cairo context adequately
-#endif
   }
 }
 
